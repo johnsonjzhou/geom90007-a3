5bdb5243be74ea6ef0ebb170a4a44994b40b1f28
Johnson Zhou <johnson@simplyuseful.io>
2022-10-17 09:13:25 +1100

    feat: cluster group override test

--- src/js/_build.js
+++ src/js/_build.js
@@ -8,13 +8,29 @@ import { bind_button_actions } from "./buttons";
 import { slider_context_labels } from "./ui.js";
 import { search_osm } from "./search.js";
 
-(($) => {
+(($, L) => {
   
   load_event_handlers();
+  
+  L.markerClusterGroup = (options) => {
+    window.clusters = new L.MarkerClusterGroup(options);
+    console.log("Clusters", window.clusters);
+    return window.clusters;
+  }
   
   window.onload = () => {
     check_user_agent();
     window.search = search_osm
+    
+    if (window.L.markerClusterGroup) {
+      window.L.markerClusterGroup = (options) => {
+        window.clusters = new L.MarkerClusterGroup(options);
+        console.log("Clusters", window.clusters);
+        return window.clusters;
+      }
+      console.log("L");
+      // Object.freeze(window.L);
+    }
   }
   
   // When shiny has loaded
@@ -23,4 +39,4 @@ import { search_osm } from "./search.js";
     slider_context_labels();
   })
   
-})($)
+})($, L)
