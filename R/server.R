eb718f2e9b29751c2017e86a74450d6e968bb8ee
jkengs <jjk47@pm.me>
2022-10-19 13:34:17 +1100

    fix: cost info formatting

--- R/server.R
+++ R/server.R
@@ -184,7 +184,7 @@ server <- function(input, output, session) {
 
     # Detail information
     location <- selected_marker$street
-    cost <- selected_marker$cost_per_hour
+    cost <- sprintf("$%.2f", selected_marker$cost_per_hour)
     start_time <- selected_marker$start_time
     end_time <- selected_marker$end_time
 
